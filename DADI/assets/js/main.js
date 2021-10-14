a88dfd943a54692ff7769858b34b2e4e662c8250
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-10-14 17:06:29 +0200

    Folder Structure

---
+++ DADI/assets/js/main.js
@@ -0,0 +1,14 @@
+/* Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte ? Se dobbiamo confrontare qualcosa che "cosa" ci serve ?
+
+
+Consigli del giorno:
+scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+si ma noi cosa vogliamo fare ? torniamo a scrivere in italiano
+proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+analizzate la traccia, per capire queli strumenti vi occorrono per risolvere il problema
+Bonus: (FATELO!)
+Usa bottone per generare il numero e poi mostriamo i risultati in pagina. */
---
+++ MAIL/assets/js/main.js
@@ -0,0 +1,22 @@
+/* Mail
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+ */
+/* Consigli del giorno:
+scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+si ma noi cosa vogliamo fare ? torniamo a scrivere in italiano
+proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+analizzate la traccia, per capire queli strumenti vi occorrono per risolvere il problema
+Bonus: (FATELO!)
+Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in pagina. */
+
+
+
+//chiedo email 
+
+//creo array di email almeno 20
+
+
+//verifico che quell'email fa parte della lista 
+
+//stampo un messaggio in console
