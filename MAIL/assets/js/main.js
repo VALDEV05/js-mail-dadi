de8b39ddc45c76587db3628886c33785a1012e0d
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-10-14 17:51:55 +0200

    Completed the reasoning and development of control

--- MAIL/assets/js/main.js
+++ MAIL/assets/js/main.js
@@ -14,15 +14,57 @@ Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in
 
 //chiedo email 
 const email = document.getElementById('email_input');
-const btn = document.getElementById('btn');
+const btn = document.getElementById('btn_generate');
 
-btn.addEventListener('click', function () {
-    console.log(email.value)
-
-})
 
 
 //creo array di email almeno 20
+//let names = ['elements', 'elements1'.......]
+
+let emailArray = [
+    'alessandrodelre@gmail.com',
+    'alessaimisciagna@gmail.com',
+    'andreabellatreccia@gmail.com',
+    'andreatosi@gmail.com',
+    'chandrarota@gmail.com',
+    'chiarapapagni@gmail.com',
+    'chiarapassaro@gmail.com',
+    'christophertarascio@gmail.com',
+    'diegodennuccio@gmail.com',
+    'doinaganceriuc@gmail.com',
+    'emanuelepica@gmail.com',
+    'fabiopacifici@gmail.com',
+    'fabiolagardin@gmail.com',
+    'federicofrascà@gmail.com',
+    'filippomangione@gmail.com',
+    'gaetanosciurti@gmail.com',
+    'valeriocorda@gmail.com',
+    'valeriocordax@gmail.com',
+    'gaianacheli@gmail.com',
+    'giovannibelda@gmail.com'
+]
+const find = document.getElementById('find')
+btn.addEventListener('click', function () {
+    if (email.value == '') {
+        const find = document.getElementById('find')
+        find.innerHTML = "scrivi un email"
+    } else {
+        for (let index = 0; index < emailArray.length; index++) {
+            const element = emailArray[index];
+            if (email.value == element) {
+                const find = document.getElementById('find')
+                find.innerHTML = "l'email è stata trovata può entrare"
+            } else {
+                const find = document.getElementById('find')
+                find.innerHTML = "email non presente nella lista"
+            }
+
+        }
+    }
+
+
+})
+
 
 
 //verifico che quell'email fa parte della lista 
