5c723d2b8eaffbc7c164def37d73ba3465307d89
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-10-14 23:52:18 +0200

    Completed MAIL layout
    I had several problems with the logic, which were fixed after several hours of debugging

--- MAIL/assets/js/main.js
+++ MAIL/assets/js/main.js
@@ -1,6 +1,8 @@
 /* Mail
 Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
  */
+
+
 /* Consigli del giorno:
 scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
 javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
@@ -10,63 +12,72 @@ analizzate la traccia, per capire queli strumenti vi occorrono per risolvere il 
 Bonus: (FATELO!)
 Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in pagina. */
 
+/* 
+    STRUMENTI
+        -.GETELEMENTBYID
+        -ARRAY
+        -.ADDEVENTLISTENER
+        -FOR LOOP
+        -.INNERHTML
+*/
 
+// Istruzioni
+const emailCheck = document.getElementById('emailCheck')
 
-//chiedo email 
-const email = document.getElementById('email_input');
-const btn = document.getElementById('btn_generate');
+const checkBtn = document.getElementById('check')
 
+const answer = document.querySelector('#risposta')
 
+const userRegistred =
+    [
+        'alessandrodelre@gmail.com',
+        'alessaimisciagna@gmail.com',
+        'andreabellatreccia@gmail.com',
+        'andreatosi@gmail.com',
+        'chandrarota@gmail.com',
+        'chiarapapagni@gmail.com',
+        'chiarapassaro@gmail.com',
+        'christophertarascio@gmail.com',
+        'diegodennuccio@gmail.com',
+        'doinaganceriuc@gmail.com',
+        'emanuelepica@gmail.com',
+        'fabiopacifici@gmail.com',
+        'fabiolagardin@gmail.com',
+        'federicofrascà@gmail.com',
+        'filippomangione@gmail.com',
+        'gaetanosciurti@gmail.com',
+        'valeriocorda@gmail.com',
+        'valeriocordax@gmail.com',
+        'gaianacheli@gmail.com',
+        'giovannibelda@gmail.com',
+        'chiara.chiacca@libero.it'
+    ]
 
-//creo array di email almeno 20
-//let names = ['elements', 'elements1'.......]
+checkBtn.addEventListener('click', function () {
 
-let emailArray = [
-    'alessandrodelre@gmail.com',
-    'alessaimisciagna@gmail.com',
-    'andreabellatreccia@gmail.com',
-    'andreatosi@gmail.com',
-    'chandrarota@gmail.com',
-    'chiarapapagni@gmail.com',
-    'chiarapassaro@gmail.com',
-    'christophertarascio@gmail.com',
-    'diegodennuccio@gmail.com',
-    'doinaganceriuc@gmail.com',
-    'emanuelepica@gmail.com',
-    'fabiopacifici@gmail.com',
-    'fabiolagardin@gmail.com',
-    'federicofrascà@gmail.com',
-    'filippomangione@gmail.com',
-    'gaetanosciurti@gmail.com',
-    'valeriocorda@gmail.com',
-    'valeriocordax@gmail.com',
-    'gaianacheli@gmail.com',
-    'giovannibelda@gmail.com'
-]
-const find = document.getElementById('find')
-btn.addEventListener('click', function () {
-    if (email.value == '') {
-        const find = document.getElementById('find')
-        find.innerHTML = "scrivi un email"
-    } else {
-        for (let index = 0; index < emailArray.length; index++) {
-            const element = emailArray[index];
-            if (email.value == element) {
-                const find = document.getElementById('find')
-                find.innerHTML = "l'email è stata trovata può entrare"
-            } else {
-                const find = document.getElementById('find')
-                find.innerHTML = "email non presente nella lista"
-            }
+    const email = emailCheck.value
+    console.log(email);
+    //varianbile "trovata"
+    let show = false
 
+    for (let i = 0; i < userRegistred.length; i++) {
+        const element = userRegistred[i];
+
+        if (email == element) {
+            show = true
         }
     }
+    /* permesso */
+    if (show == true) {
+        const allowed = `<div id="allowed" class="block">
+        <h1 class="">LA TUA EMAIL ERA REGISTRATA, CI VEDIAMO LUNEDI 18/10 ALLE 19 (ora italiana) SUL NOSTRO SITO.</h1></div>`
+        answer.innerHTML = allowed
+        /* respinto */
+    } else {
+        const rejected = `<div id="rejected" class="block">
+        <h1 class="">LA TUA EMAIL NON È STATA TROVATA NEI NOSTRI DATABESE, ALL'EVENTO MANCA MOLTO POCO, QUINDI PROVVEDI A REGISTRARE LA TUA EMAIL.</span></h1></div>`
 
-
+        answer.innerHTML = rejected
+    }
+    console.log(show);
 })
-
-
-
-//verifico che quell'email fa parte della lista 
-
-//stampo un messaggio in console
